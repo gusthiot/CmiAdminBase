7ef71c646e548035d8a71d8d60433a4af905ff1f
gusthiot <cgusthiot@gmail.com>
2017-01-10 16:49:14 +0100

    - amélioration query

--- routes/user.js
+++ routes/user.js
@@ -9,19 +9,14 @@ var con = mysql.createConnection({
     database: "cmi"
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log('Connection established');
-});
-
 router.get('/', function(req, res, next) {
     if (req.query.id) {
       var query = "SELECT comptes.ID_Compte, CONCAT(u.Labo, '_', NumeroCompte, '_', " +
           "CASE WHEN NomCompte = 'Projet par défaut' THEN u.projectnum ELSE NomCompte END) AS IntituleCompte " +
-          "FROM comptes JOIN (SELECT ID_Compte, ID_User FROM utilcomptes WHERE ID_User = '" + req.query.id + "') uc " +
-          "ON uc.ID_Compte = comptes.ID_Compte JOIN (SELECT ID_User, Labo, projectnum FROM utilisateur) u " +
+          "FROM comptes JOIN (SELECT ID_Compte, ID_User FROM utilcomptes WHERE ID_User = ?) " +
+          "uc ON uc.ID_Compte = comptes.ID_Compte JOIN (SELECT ID_User, Labo, projectnum FROM utilisateur) u " +
           "ON uc.ID_User = u.ID_User WHERE comptes.ID_Compte != 'cp0003' ORDER BY NumeroCompte";
-      con.query(query, function (err, rows) {
+      con.query(query, [req.query.id], function (err, rows) {
           if (err) throw err;
           console.log('Data received from Db !');
           console.log(req.query.id);
